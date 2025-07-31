6171c6d6c274d458647f7609b7ba8f5690255785
Adjewodo Emmanuel <emmanueladjewodo08@gmail.com>
2025-07-31 16:12:04 +0100

    updated

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +30,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <AddRecipeForm />
+      <RecipeList />
     </>
   )
 }
